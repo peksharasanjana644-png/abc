464657bb91ce50214d6acb4b696be6ff642f0ffe
peksharasanjana644-png <peksharasanjana644@gmail.com>
2026-01-03 08:55:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=bJBi1KTa#spHMeshwninvW_lXx-XmeTPJsYyhdJbGia2alfsNIfQ' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=GdJjjJxR#QXGX5IG2d1LgVtiAkaDAg9fJapQmpKukAs94DOu13xs' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
